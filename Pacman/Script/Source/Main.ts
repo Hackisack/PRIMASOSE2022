bb73545ba7e8fc8a514ea9d4985eb81bcba48125
Vinzenz Liebherr <vinzenz.liebherr@hs-furtwangen.de>
2022-03-24 15:07:18 +0100

    small changes

--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -4,7 +4,7 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
-  let speed: ƒ.Vector3 = new ƒ.Vector3(0,0,0);
+  let speed: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
 
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
 
@@ -13,7 +13,7 @@ namespace Script {
     let graph: ƒ.Node = viewport.getBranch();
     pacman = graph.getChildrenByName("Pacman")[0];
 
-    
+
 
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
@@ -23,17 +23,17 @@ namespace Script {
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
 
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && pacman.mtxLocal.translation.y%1 < 0.05){
-      speed = new ƒ.Vector3(1/60,0,0)
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.0025) % 1 < 0.05) {
+      speed.set (1 / 60, 0, 0);
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A])&& pacman.mtxLocal.translation.y%1 < 0.05){
-      speed = new ƒ.Vector3(-1/60,0,0)
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
+      speed.set(-1 / 60, 0, 0);
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W])&& pacman.mtxLocal.translation.x%1 < 0.05){
-      speed = new ƒ.Vector3(0,1/60,0)
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
+      speed.set(0, 1 / 60, 0);
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S])&& pacman.mtxLocal.translation.x%1 < 0.05){
-      speed = new ƒ.Vector3(0,-1/60,0)
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
+      speed.set(0, -1 / 60, 0);
     }
 
     pacman.mtxLocal.translate(speed);
