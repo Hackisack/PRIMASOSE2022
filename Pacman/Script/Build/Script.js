820904e51b125a06718e0bc55a3dbdc1770d4fab
Vinzenz Liebherr <vinzenz.liebherr@hs-furtwangen.de>
2022-03-29 15:48:09 +0200

    Wall functionality

--- Pacman/Script/Build/Script.js
+++ Pacman/Script/Build/Script.js
@@ -42,28 +42,85 @@ var Script;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let pacman;
+    let walls;
+    let wallcheck;
+    let direction;
     let speed = new ƒ.Vector3(0, 0, 0);
     document.addEventListener("interactiveViewportStarted", start);
     function start(_event) {
         viewport = _event.detail;
         let graph = viewport.getBranch();
         pacman = graph.getChildrenByName("Pacman")[0];
+        walls = graph.getChildrenByName("WallElements")[0].getChildren();
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.0025) % 1 < 0.05) {
-            speed.set(1 / 60, 0, 0);
+        wallcheck = false;
+        //change direction in Grid and check if direction change is valid or not
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && +(pacman.mtxLocal.translation.y.toFixed(1)) % 1 == 0) {
+            for (let x = 0; x < walls.length; x++) {
+                if (Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x - 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+                    wallcheck = true;
+                }
+            }
+            if (wallcheck == false) {
+                speed.set(1 / 60, 0, 0);
+                direction = "right";
+            }
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
-            speed.set(-1 / 60, 0, 0);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && +(pacman.mtxLocal.translation.y.toFixed(1)) % 1 == 0) {
+            for (let x = 0; x < walls.length; x++) {
+                if (Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x + 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+                    wallcheck = true;
+                }
+            }
+            if (wallcheck == false) {
+                speed.set(-1 / 60, 0, 0);
+                direction = "left";
+            }
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-            speed.set(0, 1 / 60, 0);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && +(pacman.mtxLocal.translation.x.toFixed(1)) % 1 == 0) {
+            for (let x = 0; x < walls.length; x++) {
+                if (Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y - 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+                    wallcheck = true;
+                }
+            }
+            if (wallcheck == false) {
+                speed.set(0, 1 / 60, 0);
+                direction = "up";
+            }
         }
-        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-            speed.set(0, -1 / 60, 0);
+        if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && +(pacman.mtxLocal.translation.x.toFixed(1)) % 1 == 0) {
+            for (let x = 0; x < walls.length; x++) {
+                if (Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y + 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+                    wallcheck = true;
+                }
+            }
+            if (wallcheck == false) {
+                speed.set(0, -1 / 60, 0);
+                direction = "down";
+            }
+        }
+        //check if Pacman bumps into wall
+        for (let x = 0; x < walls.length; x++) {
+            if (+(pacman.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y + 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "down") {
+                speed.set(0, 0, 0);
+                direction = "";
+            }
+            if (+(pacman.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y - 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "up") {
+                speed.set(0, 0, 0);
+                direction = "";
+            }
+            if (+(pacman.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x + 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "left") {
+                speed.set(0, 0, 0);
+                direction = "";
+            }
+            if (+(pacman.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x - 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "right") {
+                speed.set(0, 0, 0);
+                direction = "";
+            }
         }
         pacman.mtxLocal.translate(speed);
         viewport.draw();
--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -4,6 +4,9 @@ namespace Script {
 
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
+  let walls: ƒ.Node[];
+  let wallcheck: boolean;
+  let direction: string;
   let speed: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
 
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
@@ -12,9 +15,7 @@ namespace Script {
     viewport = _event.detail;
     let graph: ƒ.Node = viewport.getBranch();
     pacman = graph.getChildrenByName("Pacman")[0];
-
-
-
+    walls = graph.getChildrenByName("WallElements")[0].getChildren();
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -23,18 +24,73 @@ namespace Script {
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
 
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && (pacman.mtxLocal.translation.y + 0.0025) % 1 < 0.05) {
-      speed.set (1 / 60, 0, 0);
+    wallcheck = false;
+
+    //change direction in Grid and check if direction change is valid
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && +(pacman.mtxLocal.translation.y.toFixed(1)) % 1 ==0) {
+      for (let x = 0; x < walls.length; x++) {
+        if (Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x - 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+            wallcheck = true;
+        }
+        }
+        if (wallcheck == false) {
+          speed.set(1/ 60, 0, 0);
+          direction = "right";
+      }
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && (pacman.mtxLocal.translation.y + 0.025) % 1 < 0.05) {
-      speed.set(-1 / 60, 0, 0);
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_LEFT, ƒ.KEYBOARD_CODE.A]) && +(pacman.mtxLocal.translation.y.toFixed(1)) % 1 ==0) {
+      for (let x = 0; x < walls.length; x++) {
+        if (Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x + 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+            wallcheck = true;
+        }
+        }
+        if (wallcheck == false) {
+          speed.set(-1/60, 0, 0);
+          direction = "left";
+      }
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-      speed.set(0, 1 / 60, 0);
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_UP, ƒ.KEYBOARD_CODE.W]) && +(pacman.mtxLocal.translation.x.toFixed(1)) % 1 ==0) {
+      for (let x = 0; x < walls.length; x++) {
+        if (Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y - 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+            wallcheck = true;
+        }
+        }
+        if (wallcheck == false) {
+          speed.set(0, 1 / 60, 0);
+          direction = "up";
+      }
     }
-    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && (pacman.mtxLocal.translation.x + 0.025) % 1 < 0.05) {
-      speed.set(0, -1 / 60, 0);
+    if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_DOWN, ƒ.KEYBOARD_CODE.S]) && +(pacman.mtxLocal.translation.x.toFixed(1)) % 1 ==0) {
+      for (let x = 0; x < walls.length; x++) {
+        if (Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y + 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+            wallcheck = true;
+        }
+        }
+        if (wallcheck == false) {
+          speed.set(0, -1 / 60, 0);
+          direction = "down";
+      }
     }
+
+    //check if Pacman bumps into wall
+    for (let x = 0; x < walls.length; x++) {
+      if (+(pacman.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y + 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "down") {
+        speed.set(0, 0, 0);
+        direction = "";
+      }
+      if (+(pacman.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y - 1 && Math.round(pacman.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "up") {
+        speed.set(0, 0, 0);
+        direction = "";
+      }
+      if (+(pacman.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x + 1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "left") {
+        speed.set(0, 0, 0);
+        direction = "";
+      }
+      if (+(pacman.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x -1 && Math.round(pacman.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "right") {
+        speed.set(0, 0, 0);
+        direction = "";
+      }
+      }
 
     pacman.mtxLocal.translate(speed);
     viewport.draw();
