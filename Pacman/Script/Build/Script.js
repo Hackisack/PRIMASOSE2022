49cc22f529022a93aed25b51bb466c71b933928c
Vinzenz Liebherr <vinzenz.liebherr@hs-furtwangen.de>
2022-04-14 13:59:42 +0200

    pacman last steps

--- Pacman/Script/Build/Script.js
+++ Pacman/Script/Build/Script.js
@@ -39,24 +39,109 @@ var Script;
 var Script;
 (function (Script) {
     var ƒ = FudgeCore;
+    var ƒAid = FudgeAid;
     ƒ.Debug.info("Main Program Template running!");
     let viewport;
     let pacman;
     let walls;
     let wallcheck;
-    let direction;
+    let wallCheckMonster;
+    let direction = "";
+    let directionMonster = "";
     let speed = new ƒ.Vector3(0, 0, 0);
+    let speedMonster = new ƒ.Vector3(0, 0, 0);
+    let dialog;
+    let waka;
+    let animations;
+    let sprite;
+    let monster;
+    window.addEventListener("load", init);
     document.addEventListener("interactiveViewportStarted", start);
+    function init(_event) {
+        dialog = document.querySelector("dialog");
+        dialog.querySelector("h1").textContent = document.title;
+        dialog.addEventListener("click", function (_event) {
+            // @ts-ignore until HTMLDialog is implemented by all browsers and available in dom.d.ts
+            dialog.close();
+            startInteractiveViewport();
+        });
+        //@ts-ignore
+        dialog.showModal();
+    }
+    // setup and start interactive viewport
+    async function startInteractiveViewport() {
+        // load resources referenced in the link-tag
+        await ƒ.Project.loadResourcesFromHTML();
+        ƒ.Debug.log("Project:", FudgeCore.Project.resources);
+        // pick the graph to show
+        let graph = ƒ.Project.resources["Graph|2022-03-19T21:24:21.962Z|18014"];
+        ƒ.Debug.log("Graph:", graph);
+        if (!graph) {
+            alert("Nothing to render. Create a graph with ƒat least a mesh, material and probably some light");
+            return;
+        }
+        // setup the viewport
+        let cmpCamera = new ƒ.ComponentCamera();
+        let canvas = document.querySelector("canvas");
+        let viewport = new ƒ.Viewport();
+        viewport.initialize("InteractiveViewport", graph, cmpCamera, canvas);
+        ƒ.Debug.log("Viewport:", viewport);
+        // hide the cursor when interacting, also suppressing right-click menu
+        canvas.addEventListener("mousedown", canvas.requestPointerLock);
+        canvas.addEventListener("mouseup", function () {
+            document.exitPointerLock();
+        });
+        //setup audio
+        ƒ.AudioManager.default.listenTo(graph);
+        ƒ.Debug.log("Audio:", ƒ.AudioManager.default);
+        //load Sprite
+        let imgSpriteSheet = new ƒ.TextureImage();
+        await imgSpriteSheet.load("Textures/pacman-Sheet.png");
+        let spriteSheet = new ƒ.CoatTextured(undefined, imgSpriteSheet);
+        //generate sprite
+        animations = {};
+        let spriteName = "pacman";
+        let tempSprite = new ƒAid.SpriteSheetAnimation(spriteName, spriteSheet);
+        tempSprite.generateByGrid(ƒ.Rectangle.GET(0, 0, 64, 64), 14, 70, ƒ.ORIGIN2D.CENTER, ƒ.Vector2.X(64));
+        animations[spriteName] = tempSprite;
+        //draw viewport once for immediate feedback
+        viewport.draw();
+        canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", {
+            bubbles: true,
+            detail: viewport
+        }));
+    }
     function start(_event) {
         viewport = _event.detail;
         let graph = viewport.getBranch();
         pacman = graph.getChildrenByName("Pacman")[0];
         walls = graph.getChildrenByName("WallElements")[0].getChildren();
+        waka = graph.getChildrenByName("Sound")[0].getComponents(ƒ.ComponentAudio)[1];
+        viewport.camera.mtxPivot.translate(new ƒ.Vector3(1.5, 1.5, 15));
+        viewport.camera.mtxPivot.rotateY(180);
+        monster = createMonster();
+        graph.addChild(monster);
+        //add sprite to pacman node
+        sprite = new ƒAid.NodeSprite("Sprite");
+        sprite.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+        sprite.setAnimation(animations["pacman"]);
+        sprite.setFrameDirection(1);
+        sprite.mtxLocal.translateZ(0.5);
+        sprite.framerate = 20;
+        pacman.addChild(sprite);
+        pacman.getComponent(ƒ.ComponentMaterial).clrPrimary = new ƒ.Color(0, 0, 0, 0);
         ƒ.Loop.addEventListener("loopFrame" /* LOOP_FRAME */, update);
         ƒ.Loop.start(); // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
     }
     function update(_event) {
         // ƒ.Physics.simulate();  // if physics is included and used
+        if (direction == "") {
+            waka.play(false);
+        }
+        else if (!waka.isPlaying) {
+            waka.play(true);
+        }
+        moveMonster();
         wallcheck = false;
         //change direction in Grid and check if direction change is valid
         if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && +(pacman.mtxLocal.translation.y.toFixed(1)) % 1 == 0) {
@@ -66,7 +151,7 @@ var Script;
                 }
             }
             if (wallcheck == false) {
-                speed.set(1 / 60, 0, 0);
+                speed.set(1 / 30, 0, 0);
                 direction = "right";
             }
         }
@@ -77,7 +162,7 @@ var Script;
                 }
             }
             if (wallcheck == false) {
-                speed.set(-1 / 60, 0, 0);
+                speed.set(-1 / 30, 0, 0);
                 direction = "left";
             }
         }
@@ -88,7 +173,7 @@ var Script;
                 }
             }
             if (wallcheck == false) {
-                speed.set(0, 1 / 60, 0);
+                speed.set(0, 1 / 30, 0);
                 direction = "up";
             }
         }
@@ -99,7 +184,7 @@ var Script;
                 }
             }
             if (wallcheck == false) {
-                speed.set(0, -1 / 60, 0);
+                speed.set(0, -1 / 30, 0);
                 direction = "down";
             }
         }
@@ -122,9 +207,116 @@ var Script;
                 direction = "";
             }
         }
+        rotateSprite(direction);
         pacman.mtxLocal.translate(speed);
+        monster.mtxLocal.translate(speedMonster);
         viewport.draw();
-        ƒ.AudioManager.default.update();
+        //ƒ.AudioManager.default.update();
+    }
+    function rotateSprite(direction) {
+        if (direction == "left") {
+            if (sprite.mtxLocal.scaling.x != -1) {
+                sprite.mtxLocal.scaleX(-1);
+            }
+        }
+        if (direction == "right") {
+            if (sprite.mtxLocal.scaling.x == -1) {
+                sprite.mtxLocal.scaleX(-1);
+            }
+        }
+        if (direction == "up") {
+            if (sprite.mtxLocal.scaling.x != -1) {
+                sprite.mtxLocal.scaleX(-1);
+            }
+        }
+        if (direction == "down") {
+            if (sprite.mtxLocal.scaling.x != -1) {
+                sprite.mtxLocal.scaleX(-1);
+            }
+        }
+    }
+    function createMonster() {
+        let node = new ƒ.Node("Monster");
+        let mesh = new ƒ.MeshSphere();
+        let material = new ƒ.Material("MaterialGhost", ƒ.ShaderLit, new ƒ.CoatColored());
+        let cmpTransform = new ƒ.ComponentTransform();
+        let cmpMesh = new ƒ.ComponentMesh(mesh);
+        let cmpMaterial = new ƒ.ComponentMaterial(material);
+        cmpMaterial.clrPrimary = ƒ.Color.CSS("red");
+        node.addComponent(cmpTransform);
+        node.addComponent(cmpMesh);
+        node.addComponent(cmpMaterial);
+        node.mtxLocal.translateX(1);
+        node.mtxLocal.translateY(1);
+        return node;
+    }
+    function moveMonster() {
+        wallCheckMonster = false;
+        if (+(monster.mtxLocal.translation.x.toFixed(1)) % 1 == 0 && +(monster.mtxLocal.translation.y.toFixed(1)) % 1 == 0) {
+            let randomDirection = Math.floor(Math.random() * (3 + 1));
+            if (randomDirection == 0 && +(monster.mtxLocal.translation.x.toFixed(1)) % 1 == 0) { //up
+                for (let x = 0; x < walls.length; x++) {
+                    if (Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y + 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+                        wallCheckMonster = true;
+                    }
+                }
+                if (wallCheckMonster == false) {
+                    speedMonster.set(0, 1 / 30, 0);
+                    directionMonster = "up";
+                }
+            }
+            if (randomDirection == 1 && +(monster.mtxLocal.translation.x.toFixed(1)) % 1 == 0) { //down
+                for (let x = 0; x < walls.length; x++) {
+                    if (Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y - 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+                        wallCheckMonster = true;
+                    }
+                }
+                if (wallCheckMonster == false) {
+                    speedMonster.set(0, -1 / 30, 0);
+                    directionMonster = "down";
+                }
+            }
+            if (randomDirection == 2 && +(monster.mtxLocal.translation.y.toFixed(1)) % 1 == 0) { //left
+                for (let x = 0; x < walls.length; x++) {
+                    if (Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x + 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+                        wallCheckMonster = true;
+                    }
+                }
+                if (wallCheckMonster == false) {
+                    speedMonster.set(-1 / 30, 0, 0);
+                    directionMonster = "left";
+                }
+            }
+            if (randomDirection == 3 && +(monster.mtxLocal.translation.y.toFixed(1)) % 1 == 0) { //right
+                for (let x = 0; x < walls.length; x++) {
+                    if (Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x - 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+                        wallCheckMonster = true;
+                    }
+                }
+                if (wallCheckMonster == false) {
+                    speedMonster.set(1 / 30, 0, 0);
+                    directionMonster = "right";
+                }
+            }
+            for (let x = 0; x < walls.length; x++) {
+                if (+(monster.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y + 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "down") {
+                    speedMonster.set(0, 0, 0);
+                    direction = "";
+                }
+                if (+(monster.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y - 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "up") {
+                    speedMonster.set(0, 0, 0);
+                    direction = "";
+                }
+                if (+(monster.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x + 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "left") {
+                    speedMonster.set(0, 0, 0);
+                    direction = "";
+                }
+                if (+(monster.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x - 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "right") {
+                    speedMonster.set(0, 0, 0);
+                    direction = "";
+                }
+            }
+        }
     }
 })(Script || (Script = {}));
 //# sourceMappingURL=Script.js.map
--- Pacman/Script/Source/Main.ts
+++ Pacman/Script/Source/Main.ts
@@ -1,21 +1,112 @@
 namespace Script {
   import ƒ = FudgeCore;
+  import ƒAid = FudgeAid;
   ƒ.Debug.info("Main Program Template running!");
 
   let viewport: ƒ.Viewport;
   let pacman: ƒ.Node;
   let walls: ƒ.Node[];
   let wallcheck: boolean;
-  let direction: string;
+  let wallCheckMonster: boolean;
+  let direction: string = "";
+  let directionMonster: string = "";
   let speed: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
+  let speedMonster: ƒ.Vector3 = new ƒ.Vector3(0, 0, 0);
+  let dialog: HTMLDialogElement;
+  let waka: ƒ.ComponentAudio;
+  let animations: ƒAid.SpriteSheetAnimations;
+  let sprite: ƒAid.NodeSprite;
+  let monster: ƒ.Node;
+
+  window.addEventListener("load", init)
 
   document.addEventListener("interactiveViewportStarted", <EventListener>start);
+
+  function init(_event: Event): void {
+    dialog = document.querySelector("dialog");
+    dialog.querySelector("h1").textContent = document.title;
+    dialog.addEventListener("click", function (_event: Event) {
+      // @ts-ignore until HTMLDialog is implemented by all browsers and available in dom.d.ts
+      dialog.close();
+      startInteractiveViewport();
+    });
+    //@ts-ignore
+    dialog.showModal();
+  }
+  // setup and start interactive viewport
+  async function startInteractiveViewport(): Promise<void> {
+    // load resources referenced in the link-tag
+    await ƒ.Project.loadResourcesFromHTML();
+    ƒ.Debug.log("Project:", FudgeCore.Project.resources);
+    // pick the graph to show
+    let graph: ƒ.Graph = <ƒ.Graph>ƒ.Project.resources["Graph|2022-03-19T21:24:21.962Z|18014"];
+    ƒ.Debug.log("Graph:", graph);
+    if (!graph) {
+      alert("Nothing to render. Create a graph with ƒat least a mesh, material and probably some light");
+      return;
+    }
+    // setup the viewport
+    let cmpCamera: ƒ.ComponentCamera = new ƒ.ComponentCamera();
+    let canvas: HTMLCanvasElement = document.querySelector("canvas");
+    let viewport: ƒ.Viewport = new ƒ.Viewport();
+
+    viewport.initialize("InteractiveViewport", graph, cmpCamera, canvas);
+    ƒ.Debug.log("Viewport:", viewport);
+    // hide the cursor when interacting, also suppressing right-click menu
+    canvas.addEventListener("mousedown", canvas.requestPointerLock);
+    canvas.addEventListener("mouseup", function () {
+      document.exitPointerLock();
+    });
+    //setup audio
+    ƒ.AudioManager.default.listenTo(graph);
+    ƒ.Debug.log("Audio:", ƒ.AudioManager.default);
+
+    //load Sprite
+    let imgSpriteSheet: ƒ.TextureImage = new ƒ.TextureImage();
+    await imgSpriteSheet.load("Textures/pacman-Sheet.png");
+
+    let spriteSheet: ƒ.CoatTextured = new ƒ.CoatTextured(undefined, imgSpriteSheet)
+
+    //generate sprite
+    animations = {};
+    let spriteName: string = "pacman";
+    let tempSprite: ƒAid.SpriteSheetAnimation = new ƒAid.SpriteSheetAnimation(spriteName, spriteSheet);
+    tempSprite.generateByGrid(ƒ.Rectangle.GET(0, 0, 64, 64), 14, 70, ƒ.ORIGIN2D.CENTER, ƒ.Vector2.X(64));
+    animations[spriteName] = tempSprite;
+
+    //draw viewport once for immediate feedback
+    viewport.draw();
+    canvas.dispatchEvent(new CustomEvent("interactiveViewportStarted", {
+      bubbles: true,
+      detail: viewport
+    }));
+  }
 
   function start(_event: CustomEvent): void {
     viewport = _event.detail;
     let graph: ƒ.Node = viewport.getBranch();
     pacman = graph.getChildrenByName("Pacman")[0];
     walls = graph.getChildrenByName("WallElements")[0].getChildren();
+    waka = graph.getChildrenByName("Sound")[0].getComponents(ƒ.ComponentAudio)[1];
+
+    viewport.camera.mtxPivot.translate(new ƒ.Vector3(1.5, 1.5, 15));
+    viewport.camera.mtxPivot.rotateY(180);
+
+    monster = createMonster();
+    graph.addChild(monster);
+
+    //add sprite to pacman node
+    sprite = new ƒAid.NodeSprite("Sprite");
+    sprite.addComponent(new ƒ.ComponentTransform(new ƒ.Matrix4x4()));
+    sprite.setAnimation(<ƒAid.SpriteSheetAnimation>animations["pacman"]);
+    sprite.setFrameDirection(1);
+
+    sprite.mtxLocal.translateZ(0.5);
+    sprite.framerate = 20;
+
+    pacman.addChild(sprite);
+    pacman.getComponent(ƒ.ComponentMaterial).clrPrimary = new ƒ.Color(0, 0, 0, 0);
+
 
     ƒ.Loop.addEventListener(ƒ.EVENT.LOOP_FRAME, update);
     ƒ.Loop.start();  // start the game loop to continously draw the viewport, update the audiosystem and drive the physics i/a
@@ -24,8 +115,14 @@ namespace Script {
   function update(_event: Event): void {
     // ƒ.Physics.simulate();  // if physics is included and used
 
-    wallcheck = false;
+    if (direction == "") {
+      waka.play(false);
+    }
+    else if (!waka.isPlaying) { waka.play(true) }
 
+    moveMonster();
+
+    wallcheck = false;
     //change direction in Grid and check if direction change is valid
     if (ƒ.Keyboard.isPressedOne([ƒ.KEYBOARD_CODE.ARROW_RIGHT, ƒ.KEYBOARD_CODE.D]) && +(pacman.mtxLocal.translation.y.toFixed(1)) % 1 == 0) {
       for (let x = 0; x < walls.length; x++) {
@@ -34,7 +131,7 @@ namespace Script {
         }
       }
       if (wallcheck == false) {
-        speed.set(1 / 60, 0, 0);
+        speed.set(1 / 30, 0, 0);
         direction = "right";
       }
     }
@@ -45,7 +142,7 @@ namespace Script {
         }
       }
       if (wallcheck == false) {
-        speed.set(-1 / 60, 0, 0);
+        speed.set(-1 / 30, 0, 0);
         direction = "left";
       }
     }
@@ -56,7 +153,7 @@ namespace Script {
         }
       }
       if (wallcheck == false) {
-        speed.set(0, 1 / 60, 0);
+        speed.set(0, 1 / 30, 0);
         direction = "up";
       }
     }
@@ -67,7 +164,7 @@ namespace Script {
         }
       }
       if (wallcheck == false) {
-        speed.set(0, -1 / 60, 0);
+        speed.set(0, -1 / 30, 0);
         direction = "down";
       }
     }
@@ -91,9 +188,138 @@ namespace Script {
         direction = "";
       }
     }
-
+    rotateSprite(direction);
     pacman.mtxLocal.translate(speed);
+    monster.mtxLocal.translate(speedMonster);
     viewport.draw();
-    ƒ.AudioManager.default.update();
+    //ƒ.AudioManager.default.update();
   }
+
+  function rotateSprite(direction: string) {
+    if (direction == "left") {
+
+      if (sprite.mtxLocal.scaling.x != -1) {
+        sprite.mtxLocal.scaleX(-1);
+      }
+    }
+
+    if (direction == "right") {
+
+      if (sprite.mtxLocal.scaling.x == -1) {
+        sprite.mtxLocal.scaleX(-1);
+      }
+    }
+
+    if (direction == "up") {
+
+      if (sprite.mtxLocal.scaling.x != -1) {
+        sprite.mtxLocal.scaleX(-1);
+      }
+    }
+
+    if (direction == "down") {
+
+      if (sprite.mtxLocal.scaling.x != -1) {
+        sprite.mtxLocal.scaleX(-1);
+      }
+    }
+
+  }
+
+  function createMonster(): ƒ.Node {
+    let node: ƒ.Node = new ƒ.Node("Monster")
+
+    let mesh: ƒ.MeshSphere = new ƒ.MeshSphere();
+    let material: ƒ.Material = new ƒ.Material("MaterialGhost", ƒ.ShaderLit, new ƒ.CoatColored());
+    let cmpTransform: ƒ.ComponentTransform = new ƒ.ComponentTransform();
+    let cmpMesh: ƒ.ComponentMesh = new ƒ.ComponentMesh(mesh);
+    let cmpMaterial: ƒ.ComponentMaterial = new ƒ.ComponentMaterial(material);
+
+    cmpMaterial.clrPrimary = ƒ.Color.CSS("red");
+
+    node.addComponent(cmpTransform);
+    node.addComponent(cmpMesh);
+    node.addComponent(cmpMaterial);
+
+    node.mtxLocal.translateX(1);
+    node.mtxLocal.translateY(1);
+
+    return node;
+  }
+
+  function moveMonster(): void {
+    wallCheckMonster = false;
+    if (+(monster.mtxLocal.translation.x.toFixed(1)) % 1 == 0 && +(monster.mtxLocal.translation.y.toFixed(1)) % 1 == 0) {
+
+      let randomDirection = Math.floor(Math.random() * (3 + 1));
+      if (randomDirection == 0&& +(monster.mtxLocal.translation.x.toFixed(1)) % 1 == 0) { //up
+        for (let x = 0; x < walls.length; x++) {
+          if (Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y + 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+            wallCheckMonster = true;
+          }
+        }
+        if (wallCheckMonster == false) {
+          speedMonster.set(0, 1 / 30, 0);
+          directionMonster = "up";
+        }
+      }
+      if (randomDirection == 1 && +(monster.mtxLocal.translation.x.toFixed(1)) % 1 == 0) { //down
+        for (let x = 0; x < walls.length; x++) {
+          if (Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y - 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x) {
+            wallCheckMonster = true;
+          }
+        }
+        if (wallCheckMonster == false) {
+          speedMonster.set(0, -1 / 30, 0);
+          directionMonster = "down";
+        }
+
+      }
+      if (randomDirection == 2&& +(monster.mtxLocal.translation.y.toFixed(1)) % 1 == 0) { //left
+        for (let x = 0; x < walls.length; x++) {
+          if (Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x + 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+            wallCheckMonster = true;
+          }
+        }
+        if (wallCheckMonster == false) {
+          speedMonster.set(-1/30, 0, 0);
+          directionMonster = "left";
+        }
+
+      }
+      if (randomDirection == 3&& +(monster.mtxLocal.translation.y.toFixed(1)) % 1 == 0) { //right
+        for (let x = 0; x < walls.length; x++) {
+          if (Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x - 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y) {
+            wallCheckMonster = true;
+          }
+        }
+        if (wallCheckMonster == false) {
+          speedMonster.set(1/30, 0, 0);
+          directionMonster = "right";
+        }
+
+      }
+      for (let x = 0; x < walls.length; x++) {
+        if (+(monster.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y + 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "down") {
+          speedMonster.set(0, 0, 0);
+          direction = "";
+        }
+        if (+(monster.mtxLocal.translation.y.toFixed(1)) == walls[x].mtxLocal.translation.y - 1 && Math.round(monster.mtxLocal.translation.x) == walls[x].mtxLocal.translation.x && direction == "up") {
+          speedMonster.set(0, 0, 0);
+          direction = "";
+        }
+        if (+(monster.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x + 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "left") {
+          speedMonster.set(0, 0, 0);
+          direction = "";
+        }
+        if (+(monster.mtxLocal.translation.x.toFixed(1)) == walls[x].mtxLocal.translation.x - 1 && Math.round(monster.mtxLocal.translation.y) == walls[x].mtxLocal.translation.y && direction == "right") {
+          speedMonster.set(0, 0, 0);
+          direction = "";
+        }
+      }
+    }
+  }
+
 }
+
+
